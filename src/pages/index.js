580aabae3794df1b33cf261f1f175fb6c9643e01
Avvai Ketheeswaran <avvaik@Avvais-MacBook-Pro.local>
2020-12-21 17:17:01 -0800

    getting pages and linking to work with About page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,12 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Menu from "../components/menu"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <Menu />
     <h1>Web Developer. Designer.</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
